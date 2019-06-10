213fbc81f90703006b62d58fb360a64b52999d6c
nguyen vi nhon <nhon@carbon8.com>
2019-06-10 22:33:15 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/giaoly/precache-manifest.66c3dc0f97626c2290d0b480c8db0648.js"
+  "/giaoly/precache-manifest.7435baacf6c8d088ac6e77f8fea65ba8.js"
 );
 
 self.addEventListener('message', (event) => {
